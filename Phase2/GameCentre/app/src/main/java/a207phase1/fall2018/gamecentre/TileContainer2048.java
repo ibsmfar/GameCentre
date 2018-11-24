7c0a89cedf6f39c80b58745d6491f1ffce2f7cf8
Florin Salasan <florinsalasan@gmail.com>
2018-11-24 13:37:00 -0500

    A class used to store the information of a given tile within a saveTuple2048 container.

---
+++ Phase2/GameCentre/app/src/main/java/a207phase1/fall2018/gamecentre/TileContainer2048.java
@@ -0,0 +1,26 @@
+package a207phase1.fall2018.gamecentre;
+
+public class TileContainer2048 {
+
+    private int xPosition;
+    private int yPosition;
+    private int value;
+
+    public TileContainer2048(int xPosition, int yPosition, int value){
+        this.value = value;
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+    }
+
+    public int getXPosition() {
+        return xPosition;
+    }
+
+    public int getYPosition() {
+        return yPosition;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
