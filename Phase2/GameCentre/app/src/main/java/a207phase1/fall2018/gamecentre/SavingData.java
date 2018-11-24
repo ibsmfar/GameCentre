0c95673ca89edb0377094b0f8abce027447d3230
Florin Salasan <florinsalasan@gmail.com>
2018-11-24 14:51:46 -0500

    A class that is used to write and read to and from a file, made by Ibrahim.

---
+++ Phase2/GameCentre/app/src/main/java/a207phase1/fall2018/gamecentre/SavingData.java
@@ -0,0 +1,59 @@
+package a207phase1.fall2018.gamecentre;
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+import static android.content.Context.MODE_PRIVATE;
+
+/*
+ * The purpose of this class is to provide methods to read and write data
+ * to files.
+ */
+class SavingData {
+    final static String USER_LIST = "user_list.ser";
+    final static String ST_SCOREBOARD = "sliding_tiles_scoreboard";
+    final static String USER_SCOREBOARD = "sliding_tiles_scoreboard";
+
+    static <T> T loadFromFile(String fileName, Context context) {
+
+        try {
+            InputStream inputStream = context.openFileInput(fileName);
+            if (inputStream != null) {
+                ObjectInputStream input = new ObjectInputStream(inputStream);
+                T temp = (T) input.readObject();
+                inputStream.close();
+                return temp;
+            }
+        } catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        } catch (ClassNotFoundException e) {
+            Log.e("login activity", "File contained unexpected data type: " + e.toString());
+        }
+        return null;
+    }
+    /**
+     * Save the board manager to fileName.
+     *
+     * @param fileName the name of the file
+     */
+    static <T> void  saveToFile(String fileName, Context context, T object) {
+        try {
+            ObjectOutputStream outputStream = new ObjectOutputStream(
+                    context.openFileOutput(fileName, MODE_PRIVATE));
+            outputStream.writeObject(object);
+            outputStream.close();
+        } catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+
+}
